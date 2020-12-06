25dfda0db473c5f712e2a2fabbcae098ae7cdab2
Eric Jameson <e-jameson@bethel.edu>
2020-12-06 11:09:45 -0600

    day 2 part 2

--- day6/day6.go
+++ day6/day6.go
@@ -5,31 +5,52 @@ import (
 	"fmt"
 )
 
+func checkAnswers(personCount int, answers map[string]int) int {
+
+	count := 0
+	for _, letter := range answers {
+		if letter == personCount {
+			count += 1
+		}
+	}
+	return count
+}
+
 func main() {
 
 	lines := adventofgo.ReadFile("input.txt")
 
-	sum := 0
+	anySum, everySum, personCount := 0, 0, 0
 
-	answers := make(map[string]bool)
+	answers := make(map[string]int)
 
 	for index, line := range lines {
-		if line == ""{
-			sum += len(answers)
-			answers = make(map[string]bool)
+		if line == "" {
+			anySum += len(answers)
+			everySum += checkAnswers(personCount, answers)
+			answers = make(map[string]int)
+			personCount = 0
+			continue
 		}
 
+		personCount += 1
+
 		for _, char := range line {
-			answers[string(char)] = true
+			s := string(char)
+			answers[s] = answers[s]+1
 		}
 
 		if index == len(lines)-1 {
-			sum += len(answers)
+			anySum += len(answers)
+			everySum += checkAnswers(personCount, answers)
 		}
 	}
 
 	fmt.Println("Part 1")
-	fmt.Println(sum)
+	fmt.Println(anySum)
+
+	fmt.Println("Part 2")
+	fmt.Println(everySum)
 
 	
 }
