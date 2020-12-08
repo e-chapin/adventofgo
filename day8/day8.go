a2e3fae4789f0ceefa73abf0cc8237cfdabf0a70
Eric Jameson <e-jameson@bethel.edu>
2020-12-08 00:44:42 -0600

    day 8 part 2 and some cleanup

--- day8/day8.go
+++ day8/day8.go
@@ -7,30 +7,21 @@ import (
 	"strings"
 )
 
-
 var acc = 0
 
-
-func main() {
-
-	lines := adventofgo.ReadFile("input.txt")
-
+func runLoop(lines []string) bool{
 	instRan := make(map[int]bool)
 
 	for i:= 0; i < len(lines); i++ {
 
-
 		if instRan[i] == true {
-			fmt.Println("Part 1")
-			fmt.Println(acc)
-			break
-
+			return false
 		}
+		instRan[i] = true
 
 		line := lines[i]
-
 		opp := strings.Split(line, " ")[0]
-		
+
 		switch opp {
 		case "acc":
 			arg := strings.Split(line, " ")[1]
@@ -44,8 +35,47 @@ func main() {
 			continue
 		}
 
-		instRan[i] = true
-
 	}
+	// finished commands without repeating, no infinite loop
+	return true
 
+}
+
+
+func main() {
+
+	lines := adventofgo.ReadFile("input.txt")
+
+	fmt.Println("Part 1")
+	runLoop(lines)
+	fmt.Println(acc)
+
+	//reset for part 2
+	acc = 0
+
+	for i := 0; i < len(lines); i++ {
+
+		line := lines[i]
+		opp := strings.Split(line, " ")[0]
+
+		switch opp {
+		case "nop":
+			arg := strings.Split(line, " ")[1]
+			line = "jmp " + arg
+		case "jmp":
+			arg := strings.Split(line, " ")[1]
+			line = "nop " + arg
+		}
+
+		newLines := make([]string, len(lines))
+		copy(newLines, lines)
+		newLines[i] = line
+
+		noLoop := runLoop(newLines); if noLoop {
+			fmt.Println("Part 2")
+			fmt.Println(acc)
+		}else{
+			acc = 0
+		}
+	}
 }
