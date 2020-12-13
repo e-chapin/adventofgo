2c6efb4b3ead884780cc08b3757a078ab22e3f80
Eric Jameson <e-jameson@bethel.edu>
2020-12-13 00:52:24 -0600

    part 2. using a maths package

--- day13/day13.go
+++ day13/day13.go
@@ -3,9 +3,12 @@ package main
 import (
 	"adventofgo"
 	"fmt"
+	"github.com/deanveloper/modmath/v1/bigmod"
+	"math/big"
 	"strconv"
 	"strings"
 )
+
 
 func main() {
 
@@ -15,13 +18,10 @@ func main() {
 	departTime, _ := strconv.Atoi(strlines[0])
 	busSchedule := strlines[1]
 
-	fmt.Println(departTime)
-	fmt.Println(busSchedule)
-
 	busId := 999999
 	minBusDepartTime := 999999999
 
-	for index, val := range strings.Split(busSchedule, ",") {
+	for _, val := range strings.Split(busSchedule, ",") {
 
 		routeTime, _ := strconv.Atoi(val)
 
@@ -35,14 +35,27 @@ func main() {
 			minBusDepartTime = busDepartTime
 			busId = routeTime
 		}
-
-
-		fmt.Println(index)
-		fmt.Println(val)
 	}
 
-	fmt.Println(busId)
-	fmt.Println(minBusDepartTime - departTime)
+	fmt.Println("Day 13 Part 1")
 	fmt.Println(busId*(minBusDepartTime - departTime))
+
+	// Part 2?
+
+	var busses []bigmod.CrtEntry
+
+	for index, val := range strings.Split(busSchedule, ",") {
+		if val == "x" {
+			continue
+		}
+		busId, _ := strconv.Atoi(val)
+		busEntry := bigmod.CrtEntry{A: big.NewInt(int64(busId-index)), N: big.NewInt(int64(busId))}
+		busses = append(busses, busEntry)
+
+		fmt.Println(busId-index, busId)
+
+	}
+	fmt.Println("Day 13 part 2")
+	fmt.Println(bigmod.SolveCrtMany(busses))
 
 }
