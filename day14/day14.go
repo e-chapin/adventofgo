dc7fff92092efaadc48c8261cb803e62ecc8dac5
Eric Jameson <e-jameson@bethel.edu>
2020-12-14 01:50:44 -0600

    clean up part 1

--- day14/day14.go
+++ day14/day14.go
@@ -19,29 +19,23 @@ func main() {
 	for _, val := range strlines {
 		splLine := strings.Split(val, " = ")
 		if splLine[0] == "mask" {
-			fmt.Println("000000000000000000000000000100100000")
-			fmt.Println(splLine[1])
-
-			// first mask, both value and mask need to have 1 in each "X". Guarantees that any matching 1's from
-			// value and map are retained by X's in map, but that zeroes in map transfer to value and replace 1s.
+			// first mask, both value and mask need to have a 1, and each "X" is converted to a 1.
+			// Guarantees that any matching 1's from value and map are retained by X's in map,
+			// but that zeroes in map transfer to value and replace 1s.
 			andMask, _ = strconv.ParseInt(strings.ReplaceAll(splLine[1], "X", "1"), 2, 0)
 
-			// second mask, only one of them need to have 1 in each "X". This will guarantee all 1s in Mask will
-			// overwrite into value if that spot is a 0 in value.
+			// second mask, only one of them need to have a 1, and each "X" is converted to a 0.
+			// This will guarantee all hard 1s in Mask will overwrite into value if that spot is a 0 in value.
 			orMask, _ = strconv.ParseInt(strings.ReplaceAll(splLine[1], "X", "0"), 2, 0)
-
-			fmt.Println(strings.ReplaceAll(splLine[1], "X", "1"))
-			fmt.Println(strings.ReplaceAll(splLine[1], "X", "0"))
-
 			continue
 		}
 
 
-		// this is sad
+		// this is sad and can be improved
 		intval, _ := strconv.Atoi(splLine[1])
 		i64Val := int64(intval)
 		maxIndex := len(splLine[0])-1
-		stradr := splLine[0][4:maxIndex]
+		stradr := string(splLine[0][4:maxIndex])
 		adr, _ := strconv.Atoi(stradr)
 
 		i64Val &= andMask
@@ -55,9 +49,4 @@ func main() {
 		answer += int(v)
 	}
 	fmt.Println(answer)
-
-	x := 8
-	x |= 2
-	fmt.Println(x)
-
 }
