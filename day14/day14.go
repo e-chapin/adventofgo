1b0db48d5aed3026e2e517909ad7862337eca46e
Eric Jameson <e-jameson@bethel.edu>
2020-12-14 09:55:44 -0600

    rewrite day 14 part 1 to be more like my original idea. This is maybe less clever but easy to understand what you are counting?

--- day14/day14.go
+++ day14/day14.go
@@ -3,44 +3,29 @@ package main
 import (
 	"adventofgo"
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
 )
 
 func main() {
 
-
 	memory := make(map[int]int64)
 
 	strlines := adventofgo.ReadFile("input.txt")
-
-	var andMask int64 = 0
-	var orMask int64 = 0
+	var mask string
 	for _, val := range strlines {
 		splLine := strings.Split(val, " = ")
 		if splLine[0] == "mask" {
-			// first mask, both value and mask need to have a 1, and each "X" is converted to a 1.
-			// Guarantees that any matching 1's from value and map are retained by X's in map,
-			// but that zeroes in map transfer to value and replace 1s.
-			andMask, _ = strconv.ParseInt(strings.ReplaceAll(splLine[1], "X", "1"), 2, 0)
-
-			// second mask, only one of them need to have a 1, and each "X" is converted to a 0.
-			// This will guarantee all hard 1s in Mask will overwrite into value if that spot is a 0 in value.
-			orMask, _ = strconv.ParseInt(strings.ReplaceAll(splLine[1], "X", "0"), 2, 0)
+			mask = splLine[1]
 			continue
 		}
 
-
 		// this is sad and can be improved
-		intval, _ := strconv.Atoi(splLine[1])
-		i64Val := int64(intval)
-		maxIndex := len(splLine[0])-1
-		stradr := string(splLine[0][4:maxIndex])
-		adr, _ := strconv.Atoi(stradr)
+		value, _ := strconv.ParseInt(splLine[1], 10, 64)
 
-		i64Val &= andMask
-		i64Val |= orMask
-		memory[adr] = i64Val
+		adr, _ := strconv.Atoi(strings.TrimSuffix(strings.TrimPrefix(splLine[0], "mem["), "]"))
+		memory[adr] = maskValue(value, mask)
 
 	}
 	fmt.Println("Day 14 Part 1")
@@ -49,4 +34,26 @@ func main() {
 		answer += int(v)
 	}
 	fmt.Println(answer)
+}
+
+func maskValue(value int64, mask string) int64 {
+
+	var maskedVal int64 = 0
+	var currentBinaryValue int64
+	for index, v := range adventofgo.ReverseString(mask){
+		maskChar := string(v)
+
+		currentBinaryValue = int64(math.Pow(2, float64(index)))
+		fmt.Println(maskChar, value, currentBinaryValue)
+		if maskChar == "0" {
+			// set as 0, do nothing
+			continue
+		} else if maskChar == "X" {
+			maskedVal += value & currentBinaryValue
+		} else if maskChar == "1" {
+			//set as 1
+			maskedVal += currentBinaryValue
+		}
+	}
+	return maskedVal
 }
