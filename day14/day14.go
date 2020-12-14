0cab8cfbc10cac80173fa1b1d710fa317ba6d9ed
Eric Jameson <e-jameson@bethel.edu>
2020-12-14 01:47:37 -0600

    day 14 part 1 with some SO help

--- day14/day14.go
+++ day14/day14.go
@@ -3,111 +3,61 @@ package main
 import (
 	"adventofgo"
 	"fmt"
-	"math"
 	"strconv"
 	"strings"
 )
 
-type bits struct {
-	bitmap map[int]int
-}
-
-
-//type address struct {
-//	value []bit
-//	index int
-//}
-
 func main() {
 
 
-	address := map[int32]bits{}
-
-	for i:= 0; i < 36; i++ {
-		var b = bits{bitmap: make(map[int]int)}
-		for j:= 1; j < 34359738368; j*=2 {
-			b.bitmap[j] = 0
-		}
-		address[int32(i)] = b
-	}
-
+	memory := make(map[int]int64)
 
 	strlines := adventofgo.ReadFile("input.txt")
 
-	var mask string
-	mask = ""
+	var andMask int64 = 0
+	var orMask int64 = 0
 	for _, val := range strlines {
 		splLine := strings.Split(val, " = ")
 		if splLine[0] == "mask" {
-			fmt.Println("old mask", mask)
-			mask = splLine[1]
-			fmt.Println("new mask", mask)
+			fmt.Println("000000000000000000000000000100100000")
+			fmt.Println(splLine[1])
+
+			// first mask, both value and mask need to have 1 in each "X". Guarantees that any matching 1's from
+			// value and map are retained by X's in map, but that zeroes in map transfer to value and replace 1s.
+			andMask, _ = strconv.ParseInt(strings.ReplaceAll(splLine[1], "X", "1"), 2, 0)
+
+			// second mask, only one of them need to have 1 in each "X". This will guarantee all 1s in Mask will
+			// overwrite into value if that spot is a 0 in value.
+			orMask, _ = strconv.ParseInt(strings.ReplaceAll(splLine[1], "X", "0"), 2, 0)
+
+			fmt.Println(strings.ReplaceAll(splLine[1], "X", "1"))
+			fmt.Println(strings.ReplaceAll(splLine[1], "X", "0"))
+
 			continue
 		}
-		// mem[8] = 11
-		value, _ := strconv.Atoi(splLine[1])
+
+
+		// this is sad
+		intval, _ := strconv.Atoi(splLine[1])
+		i64Val := int64(intval)
 		maxIndex := len(splLine[0])-1
-		stradr := string(splLine[0][4:maxIndex])
+		stradr := splLine[0][4:maxIndex]
 		adr, _ := strconv.Atoi(stradr)
 
-		fmt.Println(value)
-		fmt.Println(adr)
+		i64Val &= andMask
+		i64Val |= orMask
+		memory[adr] = i64Val
 
-		newValueBits := extrapValue(value)
-		maskedBits := applyValueWithMask(newValueBits, mask)
-		address[int32(adr)] = maskedBits
 	}
 	fmt.Println("Day 14 Part 1")
-	fmt.Println(memorySum(address))
-
-}
-
-func extrapValue(value int) bits {
-
-	var b = bits{bitmap: make(map[int]int)}
-
-	for j:= 34359738368; j >= 1; j=j/2 {
-		b.bitmap[j] = 0
-		if j <= value {
-			b.bitmap[j] = 1
-			value -= j
-		}
+	answer := 0
+	for _, v := range memory {
+		answer += int(v)
 	}
-	return b
-}
+	fmt.Println(answer)
 
-func applyValueWithMask(value bits, mask string) bits{
+	x := 8
+	x |= 2
+	fmt.Println(x)
 
-	var b = bits{bitmap: make(map[int]int)}
-
-	//apply Mask to value
-	mask = adventofgo.ReverseString(mask)
-	for j := 1; j < 34359738368; j*=2 {
-		bitIndex := int(math.Log2(float64(j)))
-
-		if string(mask[bitIndex]) == "X" {
-			b.bitmap[j] = value.bitmap[j]
-			continue
-		} else {
-			newValStr := string(mask[bitIndex])
-			newVal, _ := strconv.Atoi(newValStr)
-			b.bitmap[j] = newVal
-		}
-	}
-
-	return b
-}
-
-func memorySum(address map[int32]bits) int {
-
-	var sum int
-	sum = 0
-	for i:= 0; i < 36; i++ {
-		for j:= 1; j < 34359738368; j*=2 {
-			if address[int32(i)].bitmap[j] == 1 {
-				sum += j
-			}
-		}
-	}
-	return sum
 }
