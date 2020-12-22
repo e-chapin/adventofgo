735af4b9c2b1fe5627e3bb69a43ce0c2efaab027
Eric Jameson <e-jameson@bethel.edu>
2020-12-22 12:17:42 -0600

    clean up

--- day22/day22.go
+++ day22/day22.go
@@ -17,6 +17,13 @@ func getDeck(input string) (deck []int) {
 	return
 }
 
+func getSubDeck(deck []int, card int) (sub []int) {
+	for i := 0; i < card; i++ {
+		sub = append(sub, deck[i])
+	}
+	return
+}
+
 func CalculateScore(deck []int) (score int){
 	for i, v := range deck {
 		factor := len(deck) - i
@@ -53,23 +60,19 @@ func playGame(p1, p2 []int, game int) int {
 		}
 
 		c1, c2 := p1[0], p2[0]
-		p1 = p1[1:]
-		p2 = p2[1:]
+		p1, p2 = p1[1:], p2[1:]
+
 
 		// game -1 means this is part one, and we aren't playing Recursive Combat
-		if c1 <= len(p1) && c2 <= len(p2) && game > 0{
+		// If both player 1 and 2 have *equal or more* cards in their deck than the card they drew
+		// then play a sub-game of Recursive Combat
+		if len(p1) >= c1 && len(p2) >= c2 && game > 0 {
 
-			var s1 []int
-			for i := 0; i < c1; i++ {
-				s1 = append(s1, p1[i])
-			}
-
-			var s2 []int
-			for i := 0; i < c2; i++ {
-				s2 = append(s2, p2[i])
-			}
+			s1 := getSubDeck(p1, c1)
+			s2 := getSubDeck(p2, c2)
 			winner = playGame(s1, s2, game+1)
 
+		// otherwise just play a round of normal Combat
 		} else {
 			if c1 > c2 {
 				winner = 1
@@ -78,14 +81,12 @@ func playGame(p1, p2 []int, game int) int {
 			}
 		}
 
-		if winner == 1{
+		if winner == 1 {
 			// Player 1 wins. add cards to bottom of player 1 deck, keeping c1 and then adding c2.
-			p1 = append(p1, c1)
-			p1 = append(p1, c2)
+			p1 = append(p1, c1, c2)
 		} else {
 			// player 2 wins, do opposite.
-			p2 = append(p2, c2)
-			p2 = append(p2, c1)
+			p2 = append(p2, c2, c1)
 		}
 	}
 
